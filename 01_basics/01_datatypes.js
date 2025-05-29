51bb397038156ec8008c00ec25550e6078d94c26
HOMSHREE <135824277+HOMSHREE@users.noreply.github.com>
2025-05-29 00:23:47 +0000

    commit done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,17 @@
+"use script"; //treat all JS code as neweer version
+
+//number => 2 pwer 53
+//bigint 
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value not assigned
+//symbol => unique
+
+// object
+
+console.log(typeof "hom");
+console.log(typeof undefined);// undefined
+console.log(typeof null); // object 
+console.log(typeof {we : 1234});
+
