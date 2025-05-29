fb75e84d922a2be9f33953bf14a426621438ff73
HOMSHREE <135824277+HOMSHREE@users.noreply.github.com>
2025-05-29 00:23:10 +0000

    commit done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 1122334
+let accountEmail = "hom@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bangalore"
+let accountState
+
+// accountId = 2
+
+accountEmail = "hc@hc.com"
+accountPassword = "232323"
+accountCity = "dddd"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
+
+/*
+prefer not to use var
+because of blocke level ND FUNCTION LEVEL SCOPE
+*/
