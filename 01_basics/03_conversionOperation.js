80c3dc1c72821ccc1844d1fda07e85f2e9e6dd41
HOMSHREE <135824277+HOMSHREE@users.noreply.github.com>
2025-06-05 00:45:41 +0000

    commit done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -4,13 +4,26 @@
 var score = true // converts to 1
 
 
-console.log(typeof score); 
+// console.log(typeof score); 
 
 var valueInNumber = Number(score); // Converts string to number
-console.log(typeof valueInNumber); 
-console.log(valueInNumber);  // prints NaN (Not a Number) because the string contains non-numeric characters
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber);  // prints NaN (Not a Number) because the string contains non-numeric characters
 
+// "33" => 33
+// "33asd" => NaN
+// true => 1 & false => 0
 
 let isLoggedIn = 1; 
 
 let booleanIsLoggedIn = Boolean(isLoggedIn); // Converts number to boolean
+// console.log(booleanIsLoggedIn); // prints true
+
+// 1 => true
+// 0 => false
+// "" => false
+// "Hom" => true
+
+let someNumber = 33;
+let someString = String(someNumber); // Converts number to string
+console.log(typeof someString); // prints "33"
